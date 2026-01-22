da5e272e3d67cb5b365811c652840fba35e58073
Damaris <damaris.ionel@emanuel.ro>
2026-01-22 11:26:53 +0200

    Refactor code structure for improved readability and maintainability

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,5 +1,1 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/ProiectAN.TAW/sw.js", { scope: "/ProiectAN.TAW/" })
-  })
-}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
